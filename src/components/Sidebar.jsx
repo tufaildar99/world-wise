ef26c060055dd7d2109cbf94dbcfec31b84b6b6c
Tufail Dar <tufaildar2016@gmail.com>
2024-02-22 11:04:13 +0530

    Add nested routes for cities, countries, and form in App.jsx and update AppNav.jsx and Sidebar.jsx to reflect the changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
+import { Outlet } from "react-router-dom";
 
 import AppNav from "./AppNav";
 
@@ -8,7 +9,7 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by Worldwise
